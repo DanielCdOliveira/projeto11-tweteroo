774c8038beabbffe51a08ff505fe0a8ffafadea7
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-04-24 16:07:46 -0300

    Json fix

--- Back/index.js
+++ Back/index.js
@@ -2,12 +2,11 @@ import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
 const users = [];
 const tweets = [];
-
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
